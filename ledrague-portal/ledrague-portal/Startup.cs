dad8e9ae982ca053c56bde351e44eb19b4d9d930
rejean Caron <rejean.caron@oracle.com>
2017-11-25 10:53:21 -0500

    Services refactoring

--- ledrague-portal/ledrague-portal/Startup.cs
+++ ledrague-portal/ledrague-portal/Startup.cs
@@ -33,14 +33,47 @@ namespace ledrague_portal
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 2;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 1;
+            });
 
-            services.AddMvc();
+            /*
+            services.AddAuthorization(options =>
+            {
+               options.AddPolicy("test1", policy => policy.RequireRole("admin"));
+            });
+            */
+
+            services.AddCors(options => options.AddPolicy("CorsPolicy",
+                builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+                }));
+
+            services.AddMvc()
+               .AddRazorPagesOptions(options =>
+               {
+                   options.Conventions.AuthorizeFolder("/Account/Manage");
+                   options.Conventions.AuthorizePage("/Account/Logout");
+               })
+               .AddJsonOptions(
+                   options => options.SerializerSettings.ReferenceLoopHandling =
+                   Newtonsoft.Json.ReferenceLoopHandling.Ignore
+           );
+
+            // Register no-op EmailSender used by account confirmation and password reset during development
+            // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=532713
+            services.AddSingleton<IEmailSender, EmailSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -50,18 +83,34 @@ namespace ledrague_portal
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
             }
 
             app.UseStaticFiles();
 
             app.UseAuthentication();
 
+            
+            /*
+            // Seed Roles for the application
+            string[] applicationRoles = new String[] { "poweruser", "admin", "animation", "dj" };
+            foreach (String roleName in applicationRoles)
+            {
+                Task<IdentityRole> role = roleManager.FindByNameAsync(roleName);
+                if (role.Result == null)
+                {
+                    roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+            */
+
+            app.UseCors("CorsPolicy");
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller}/{action=Index}/{id?}");
             });
         }
     }
